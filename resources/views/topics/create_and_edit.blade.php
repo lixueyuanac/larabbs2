011ad1c8c6f7e56e5ba220a7e14d57cad9718135
vagrant <vagrant@homestead>
2018-08-02 03:26:33 +0000

    WYSWYG Editor

--- resources/views/topics/create_and_edit.blade.php
+++ resources/views/topics/create_and_edit.blade.php
@@ -23,14 +23,15 @@
                     @if($topic->id)
                         <form action="{{ route('topics.update', $topic->id) }}" method="POST" accept-charset="UTF-8">
                             <input type="hidden" name="_method" value="PUT">
-                            @else
-                                <form action="{{ route('topics.store') }}" method="POST" accept-charset="UTF-8">
-                                    @endif
+                    @else
+                        <form action="{{ route('topics.store') }}" method="POST" accept-charset="UTF-8">
+                    @endif
 
                                     <input type="hidden" name="_token" value="{{ csrf_token() }}">
 
                                     <div class="form-group">
-                                        <input class="form-control" type="text" name="title" value="{{ old('title', $topic->title ) }}" placeholder="请填写标题" required/>
+                                        <input class="form-control" type="text" name="title"
+                                               value="{{ old('title', $topic->title ) }}" placeholder="请填写标题" required/>
                                     </div>
 
                                     <div class="form-group">
@@ -43,16 +44,38 @@
                                     </div>
 
                                     <div class="form-group">
-                                        <textarea name="body" class="form-control" id="editor" rows="3" placeholder="请填入至少三个字符的内容。" required>{{ old('body', $topic->body ) }}</textarea>
+                                        <textarea name="body" class="form-control" id="editor" rows="3"
+                                                  placeholder="请填入至少三个字符的内容。"
+                                                  required>{{ old('body', $topic->body ) }}</textarea>
                                     </div>
 
                                     <div class="well well-sm">
-                                        <button type="submit" class="btn btn-primary"><span class="glyphicon glyphicon-ok" aria-hidden="true"></span> 保存</button>
+                                        <button type="submit" class="btn btn-primary"><span
+                                                    class="glyphicon glyphicon-ok" aria-hidden="true"></span> 保存
+                                        </button>
                                     </div>
                                 </form>
                 </div>
             </div>
         </div>
     </div>
+@section('styles')
+    <link rel="stylesheet" type="text/css" href="{{ asset('css/simditor.css') }}">
+@stop
 
+@section('scripts')
+    <script type="text/javascript"  src="{{ asset('js/module.js') }}"></script>
+    <script type="text/javascript"  src="{{ asset('js/hotkeys.js') }}"></script>
+    <script type="text/javascript"  src="{{ asset('js/uploader.js') }}"></script>
+    <script type="text/javascript"  src="{{ asset('js/simditor.js') }}"></script>
+
+    <script>
+        $(document).ready(function(){
+            var editor = new Simditor({
+                textarea: $('#editor'),
+            });
+        });
+    </script>
+
+@stop
 @endsection
